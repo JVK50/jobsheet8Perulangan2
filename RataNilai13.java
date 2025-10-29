17c46b802da0b0b9faa526101a5ca3199da18ab4
JVK50 <jidanali32@gmail.com>
2025-10-29 20:53:16 +0700

    Percobaan

---
+++ RataNilai13.java
@@ -0,0 +1,9 @@
+package jobsheet8Loop.jobsheet8Perulangan2;
+import java.util.Scanner;
+
+public class RataNilai13 {
+    public static void main(String[] args) {
+        Scanner acc = new Scanner(System.in);
+        
+    }
+}
---
+++ star13.java
@@ -0,0 +1,5 @@
+package jobsheet8Loop.jobsheet8Perulangan2;
+
+public class star13 {
+    
+}
