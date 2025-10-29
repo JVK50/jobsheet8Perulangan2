2b153dea1c6f7d0cbf477efdeea07ebb08c7f1b0
JVK50 <jidanali32@gmail.com>
2025-10-29 23:53:18 +0700

    Percobaan 2

--- square13.java
+++ square13.java
@@ -1,5 +1,20 @@
 package jobsheet8Loop.jobsheet8Perulangan2;
 
+import java.util.Scanner;
+
 public class square13 {
-    
+    public static void main(String[] args) {
+        Scanner acc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = acc.nextInt();
+
+        for (int a = 1; a <= N; a++) {
+            for (int i = 1; i <= N; i++) {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+        acc.close();
+    }
 }
