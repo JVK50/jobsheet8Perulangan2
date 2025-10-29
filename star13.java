51553d8f281f1bab3b44c1d75dffe325fa90ea6b
JVK50 <jidanali32@gmail.com>
2025-10-29 22:49:28 +0700

    Percobaan 1

--- star13.java
+++ star13.java
@@ -1,5 +1,16 @@
 package jobsheet8Loop.jobsheet8Perulangan2;
+import java.util.Scanner;
 
 public class star13 {
-    
+     public static void main(String[] args) {
+        Scanner acc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = acc.nextInt();
+
+        for (int i = 1; i <= N; i--) {
+            System.out.print("*");
+        }
+        acc.close();
+    }   
 }
