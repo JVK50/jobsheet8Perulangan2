879f953ac1cfd0d9c68096a1ac624d51fcb3229e
JVK50 <jidanali32@gmail.com>
2025-10-30 16:53:37 +0700

    Percobaan 4

--- RataNilai13.java
+++ RataNilai13.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 public class RataNilai13 {
     public static void main(String[] args) {
         Scanner acc = new Scanner(System.in);
+
+        int i = 1, j;
+        float nilai, totalNilai, rataNilai;
+
+        while (i <= 5) {
+            totalNilai = 0;
+            System.out.println("Input nilai siswa ke-" + i);
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Masukkan nilai ke-" + j + " : ");
+                nilai = acc.nextFloat();
+                totalNilai += nilai;
+                }
+                rataNilai = totalNilai / 5;
+                System.out.println("Rata-rata nilai siswa ke-" + i + " adalah: " + rataNilai);
+                i++;
+            }
         
+        
+        }
     }
-}
+
