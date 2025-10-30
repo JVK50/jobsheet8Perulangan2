b78682b2f542fc9213aba775a64f40f272619319
JVK50 <jidanali32@gmail.com>
2025-10-30 16:55:56 +0700

    Percobaan 3

--- triangle13.java
+++ triangle13.java
@@ -1,5 +1,22 @@
 package jobsheet8Loop.jobsheet8Perulangan2;
+import java.util.Scanner;
 
 public class triangle13 {
-    
+    public static void main(String[] args) {
+        Scanner apt = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int N = apt.nextInt();
+        int i = 0;
+
+        while (i <= N) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+       apt.close(); 
+    }    
 }
