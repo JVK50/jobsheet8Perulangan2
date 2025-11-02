abbe7f723579cf9434fbbd71fa8f81e8b38bc25b
JVK50 <jidanali32@gmail.com>
2025-11-02 21:18:36 +0700

    Tugas jobsheet 8

---
+++ Tugas/Tugas1_13.java
@@ -0,0 +1,15 @@
+package jobsheet8Loop.jobsheet8Perulangan2.Tugas;
+import java.util.Scanner;
+
+public class Tugas1_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        System.out.print("Masukkan nilai N: ");
+        N = sc.nextInt();
+
+        for (int i = 1; i <= N ; i++) {
+                
+        }
+    }
+}
